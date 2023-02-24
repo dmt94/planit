4c814c44148c28ed95f40f863a7ed40a21ffc0ae
Daevah Taylaran <daevtayl@gmail.com>
2023-02-23 22:14:19 -0800

    update oauth

---
+++ config/passport.js
@@ -0,0 +1,38 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const User = require('../models/user');
+
+passport.use(new GoogleStrategy(
+  // Configuration object
+  {
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK
+  },
+
+  async function(accessToken, refreshToken, profile, cb) {
+    try {
+      let user = await User.findOne({ googleId: profile.id })
+      if (user) return cb(null, user);
+      user = await User.create({
+        name: profile.displayName,
+        googleId: profile.id,
+        email: profile.emails[0].value,
+        avatar: profile.photos[0].value,
+      });
+
+      return cb(null, user);
+
+    } catch (err) {
+      return cb(err);
+    }
+  }
+));
+
+passport.serializeUser(function(user, cb) {
+  cb(null, user._id);
+});
+
+passport.deserializeUser(async function(userId, cb) {
+  cb(null, await User.findById(userId));
+});
--- server.js
+++ server.js
@@ -3,9 +3,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+const passport = require('passport');
+var methodOverride = require('method-override');
+
+
 require('dotenv').config();
 require('./config/database');
-var methodOverride = require('method-override');
+require('./config/passport');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -22,6 +27,21 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+/* Makes sure that the logged in user is in a user variable that's available inside all EJS templates. If nobody is logged in, user will be undefined. */
+app.use(function (req, res, next) {
+  res.locals.user = req.user;
+  next();
+});
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);

5c539620ccfa497e5730e56600421dfd004a3641
Daevah Taylaran <daevtayl@gmail.com>
2023-02-23 21:05:31 -0800

    connect to mongoose

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);

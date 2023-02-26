42cbcb88e57f1e101ab08095f54917c0891c6a02
Daevah Taylaran <daevtayl@gmail.com>
2023-02-26 13:31:10 -0800

    check for existing date in mongoose

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,8 +1,10 @@
 const User = require('../models/user');
 
+
 module.exports = {
   index,
-  show
+  show,
+  new: newDateEvent
 }
 
 function index(req, res) {
@@ -15,13 +17,24 @@ function index(req, res) {
 }
 
 function show(req, res) {
-  User.findOne(req.user, function(err, user) {
-    console.log("User name", user.name);
-    console.log("query", req.query);
-    res.render('dashboard/show', {
-      date: req.query.date,
-      user: user,
-      title: 'Dashboard'
-    })
+  let date = req.query.date.split('-');
+  let dateObj = new Date(date[0], Number(date[1]) - 1, date[2]);
+  
+  User.findOne({
+    'date.date': dateObj
+  }).then(function(user) {
+    if (!user) {
+      console.log("query", req.query);
+      
+      res.render('dashboard/show', {
+        date: req.query.date,
+        user: req.user,
+        title: 'Dashboard'
+      })
+    }
   })
+}
+
+function newDateEvent(req, res) {
+
 }

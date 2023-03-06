19a0855d2ddee85e3fe84c792bc4bc35b74bf77d
Daevah Taylaran <daevtayl@gmail.com>
2023-03-05 19:30:44 -0800

    changed date

--- controllers/askgpt.js
+++ controllers/askgpt.js
@@ -12,11 +12,12 @@ module.exports = {
     let user = req.user;
     let events = await Event.find({user: req.user});
     console.log(events);
+    let today = new Date();
   
     let message = req.body.message;
     const response = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: `You are a personal assistant. Give suggestions, locations, tasks, events if asked. Present lists in a bullet format. Greet with a message for ${user}. Look up ${events} and its properties if asked. Give time in 12 hour format. >${message}?`,
+      prompt: `You are a personal assistant. Give suggestions, locations, tasks, events if asked. Present lists in a bullet format. Greet with a message for ${user}. Look up ${events} and its properties if asked. Refer to ${today} if asked about date or time. >${message}?`,
       max_tokens: 300,
       temperature: 0.45,
     });
